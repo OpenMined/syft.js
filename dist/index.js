ae7911d356c2aefe17ceb237b9469fd4e2decc64
Michaelangelo Jong <mike96jong@gmail.com>
2018-01-06 20:59:54 -0700

    npm package stuff

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+tslib_1.__exportStar(require("./controller"), exports);
+tslib_1.__exportStar(require("./Tensor"), exports);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from './controller'
+export * from './Tensor'
+// export * from './nn'
