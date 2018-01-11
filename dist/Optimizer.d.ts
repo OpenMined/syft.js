a4b4ec35d1ca252a8d94cf2f09d73caa95526d41
Michaelangelo Jong <mike96jong@gmail.com>
2018-01-11 16:12:41 -0700

    Optimizer

---
+++ dist/Optimizer.d.ts
@@ -0,0 +1,26 @@
+import { AsyncInit, IAsyncInit } from './AsyncInit';
+export declare class Optimizer extends AsyncInit implements IAsyncInit {
+    id: string;
+    type: string;
+    optimizer_type: string;
+    constructor(id?: string, optimizer_type?: string, params?: any[], h_params?: any[]);
+    finish(id: string): void;
+    zero_grad(): Promise<any>;
+    step(batch_size: number, iteration: number): Promise<any>;
+    cmd(function_call: string, params?: any[], h_params?: any[]): {
+        functionCall: string;
+        objectType: string;
+        objectIndex: string;
+        tensorIndexParams: any[];
+        hyperParams: any[];
+    };
+}
+export declare class SGD extends Optimizer {
+    constructor(params: any[], lr?: number, momentum?: number, decay?: number);
+}
+export declare class RMSProp extends Optimizer {
+    constructor(params: any[], lr?: number, rho?: number, epsilon?: number, decay?: number);
+}
+export declare class Adam extends Optimizer {
+    constructor(params: any[], lr?: number, beta_1?: number, beta_2?: number, epsilon?: number, decay?: number);
+}
