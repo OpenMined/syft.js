1b61e9cc024b19dc3f17226185afc27bcce1e631
Michaelangelo Jong <mike96jong@gmail.com>
2018-04-05 14:52:18 -0300

    doc links

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -rf dist;
 rm -rf docs;
-tsc && find src -name '*.ts' | xargs standardts && typedoc --theme minimal src/ --out docs;
+tsc && find src -name '*.ts' | xargs standardts && typedoc --gitRevision $(git log -n 1 --pretty=format:"%H") --theme minimal src/ --out docs;
