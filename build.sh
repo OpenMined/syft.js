889099afc15c9109d99f86f0f7489e32306867fe
Michaelangelo Jong <mike96jong@gmail.com>
2018-04-05 15:06:15 -0300

    gh-pages

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 rm -rf dist;
 rm -rf docs;
 tsc && find src -name '*.ts' | xargs standardts && typedoc --gitRevision $(git log -n 1 --pretty=format:"%H") --theme minimal src/ --out docs;
+touch ./docs/.nojekyll
