e33e50c6f1dd52dfa1d25d9cdc38406710f35a42
cereallarceny <me@patrickcason.com>
2018-07-23 16:04:00 -0300

    Changing to JSON

--- examples/convert-json-to-tensor/script.js
+++ examples/convert-json-to-tensor/script.js
@@ -1,12 +1,13 @@
-var tensorObject = {
-  torch_type: 'torch.FloatTensor',
-  data: [1.0, 2.0, 3.0, 4.0, 5.0],
-  id: 1476041248,
-  owners: [0],
-  is_pointer: false
-};
+const json = `{
+  "torch_type": "torch.FloatTensor",
+  "data": [1.0, 2.0, 3.0, 4.0, 5.0],
+  "id": 1476041248,
+  "owners": [0],
+  "is_pointer": false
+}`;
 
-var floatTensor = new Syft(tensorObject);
+const tensorObject = JSON.parse(json);
+const floatTensor = new Syft(tensorObject);
 
 floatTensor.torch_type = 'test';
 
