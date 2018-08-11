b1cd0242f77a996d02d57859c9fcad00269cfba5
Akil M Hylton <hyltonakil@gmail.com>
2018-08-11 15:46:27 -0400

    Update rollup.config.js
    Improve build system

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
+// Rollup plugins 
 import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/index.js',
-  output: {
+  output: [{
     file: 'lib/index.js',
     name: 'Syft',
     format: 'iife',
     sourceMap: true
   },
+  {
+    file: 'lib/index.esm.js',
+    format: format: 'esm'
+  } 
+],
   plugins: [
     babel({
       exclude: 'node_modules/**'
