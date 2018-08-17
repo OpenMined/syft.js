36622e00ad2dce6febb8c60e339bf06864c37c0d
cereallarceny <me@patrickcason.com>
2018-08-17 18:56:32 -0500

    Closes #32 by adding sourcemaps and minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,26 @@
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.js',
-  output: [{
-    file: 'lib/index.js',
-    name: 'Syft',
-    format: 'umd',
-    sourceMap: true
-  },
-  {
-    file: 'lib/index.esm.js',
-    name: 'Syft',
-    format: 'esm',
-    sourceMap: true
-  } 
-],
+  output: [
+    {
+      file: 'lib/index.js',
+      name: 'Syft',
+      format: 'umd',
+      sourcemap: true
+    },
+    {
+      file: 'lib/index.esm.js',
+      name: 'Syft',
+      format: 'esm',
+      sourcemap: true
+    }
+  ],
   plugins: [
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    terser()
   ]
 };
