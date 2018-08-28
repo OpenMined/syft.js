3c2d94bd061a1f8f0dd21ee1aab04d6da775a03e
Patrick Cason <me@patrickcason.com>
2018-08-28 15:23:30 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import resolve from 'rollup-plugin-node-resolve';
 
-
 export default {
   input: 'src/index.js',
   output: [
