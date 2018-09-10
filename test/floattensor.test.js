61758756bce1e2cc8755717efb9323d72e1c4d39
cereallarceny <me@patrickcason.com>
2018-09-09 20:08:49 -0500

    Removing irrelevant tests

--- test/floattensor.test.js
+++
@@ -1,8 +0,0 @@
-const FloatTensor = require('./floattensor');
-const addition = require('./floattensor');
-
-
-test('Adds 2.9 + 1.0 to equal 3.9', () => {
-    expect(addition(2.9, 1.0)).toBe(3.9);
-});
-
