9a8f917fdfe17695db51d41ad4db18d443d63c39
cereallarceny <me@patrickcason.com>
2019-07-31 16:43:09 +0100

    Serde is improved along the current version of PySyft, only have arbitrary commas remaining

--- src/serde.js
+++ src/serde.js
@@ -1,12 +1,6 @@
 /*
-  TODO: There are currently two known issues with simplifiers:
-  - The "shape" property inside of PointerTensor insists on being converted to a list, but this isn't done in PySyft
-  - PySyft seems to have some way of deciding between square brackets ("[]") and parentheses ("()"), it's unclear which we should use and when
-
-  TODO: There is one known bug in how PySyft is coercing string values:
-  - Currently, PySyft is inserting "b" before a string and occasionally "," after a string because of how MsgPack is serializing data
-  - Neither of these characters are necessary, and hopefully at some point we can remove these extraneous details
-  - Upon fixing this in the SIMPLIFIERS array, we will also need to fix this in the REPLACERS array
+  TODO: Arbitrary commas are being inserted by PySyft at various places, usually at the end of strings, tuples, and TorchSize. They don't do anything and should not be there, this is an issue with PySyft
+  TODO: Let's look at optimization potentials by using Map() (with the key being the instanceof class) for the SIMPLIFIERS and DETAILERS variables
 */
 
 // Import our types
@@ -14,6 +8,7 @@ import Range from './custom-types/range';
 import Slice from './custom-types/slice';
 import Tuple from 'immutable-tuple';
 import TorchTensor from './custom-types/torch-tensor';
+import TorchSize from './custom-types/torch-size';
 import Plan from './custom-types/plan';
 import PointerTensor from './custom-types/pointer-tensor';
 
@@ -57,58 +52,51 @@ const runReplacers = (data, replacers) => {
 // 2. Depending on the data type, we may run it through a simplifier function to convert that value into a string
 // 3. Run the replacers, which will convert any Javascript values into Python (null, false, true, etc.)
 export const simplify = data => {
-  const SIMPLIFIERS = [
-    d =>
-      `(0, [${Array.from(d)
+  const SIMPLIFIERS = {
+    dict: d =>
+      `(0, (${Array.from(d)
         .map(([key, value]) => `(${parse(key)}, ${parse(value)})`)
-        .join()}])`, // 0 = dict
-    d => `(1, [${d.map(i => parse(i)).join()}])`, // 1 = list
-    d => `(2, (${d.start}, ${d.end}, ${d.step}))`, // 2 = range
-    d => `(3, [${[...d].map(i => parse(i)).join()}])`, // 3 = set
-    d => `(4, (${d.start}, ${d.end}, ${d.step}))`, // 4 = slice
-    d => `(5, (b'${d}',))`, // 5 = str
-    d => `(6, [${d.map(i => parse(i)).join()}])`, // 6 = tuple
-    null, // 7
-    null, // 8
-    null, // 9
-    null, // 10
-    null, // 11
-    d =>
+        .join()}))`,
+    list: d => `(1, (${d.map(i => parse(i)).join()}))`,
+    range: d => `(2, (${d.start}, ${d.end}, ${d.step}))`,
+    set: d => `(3, (${[...d].map(i => parse(i)).join()}))`,
+    slice: d => `(4, (${d.start}, ${d.end}, ${d.step}))`,
+    str: d => `(5, (b'${d}'))`,
+    tuple: d => `(6, (${d.map(i => parse(i)).join()}))`,
+    torchTensor: d =>
       `(12, (${getArgs(TorchTensor)
         .map(i => parse(d[i]))
-        .join()}))`, // 12 = torch-tensor
-    null, // 13
-    null, // 14
-    null, // 15
-    null, // 16
-    d => {
+        .join()}))`,
+    torchSize: d => `(13, (${d.size}))`,
+    plan: d => {
       const args = getArgs(Plan),
         operations = d[args[0]].map(i => parse(i)),
         rest = args.slice(1).map(i => parse(d[i]));
 
-      return `(17, ((${operations}), ${rest.join()}))`;
-    }, // 17 = plan
-    d =>
-      `(18, (${getArgs(PointerTensor)
+      return `(18, ((${operations}), ${rest.join()}))`;
+    },
+    pointerTensor: d =>
+      `(19, (${getArgs(PointerTensor)
         .map(i => parse(d[i]))
         .join()}))` // 18 = pointer-tensor
-  ];
+  };
 
   const parse = d => {
     if (d === null) return 'null';
 
     let simplifierId = null;
 
-    if (d instanceof Map) simplifierId = 0;
-    else if (d instanceof Array) simplifierId = 1;
-    else if (d instanceof Range) simplifierId = 2;
-    else if (d instanceof Set) simplifierId = 3;
-    else if (d instanceof Slice) simplifierId = 4;
-    else if (typeof d === 'string') simplifierId = 5;
-    else if (d instanceof Tuple) simplifierId = 6;
-    else if (d instanceof TorchTensor) simplifierId = 12;
-    else if (d instanceof Plan) simplifierId = 17;
-    else if (d instanceof PointerTensor) simplifierId = 18;
+    if (d instanceof Map) simplifierId = 'dict';
+    else if (d instanceof Array) simplifierId = 'list';
+    else if (d instanceof Range) simplifierId = 'range';
+    else if (d instanceof Set) simplifierId = 'set';
+    else if (d instanceof Slice) simplifierId = 'slice';
+    else if (typeof d === 'string') simplifierId = 'str';
+    else if (d instanceof Tuple) simplifierId = 'tuple';
+    else if (d instanceof TorchTensor) simplifierId = 'torchTensor';
+    else if (d instanceof TorchSize) simplifierId = 'torchSize';
+    else if (d instanceof Plan) simplifierId = 'plan';
+    else if (d instanceof PointerTensor) simplifierId = 'pointerTensor';
 
     if (simplifierId !== null) {
       const simplifier = SIMPLIFIERS[simplifierId];
@@ -147,12 +135,13 @@ export const detail = data => {
     null, // 10
     null, // 11
     d => new TorchTensor(...d.map(i => parse(i))), // 12 = torch-tensor
-    null, // 13
+    d => new TorchSize(d), // 13 = torch.Size
     null, // 14
     null, // 15
     null, // 16
-    d => new Plan(d[0].map(j => parse(j)), ...d.slice(1).map(i => parse(i))), // 17 = plan
-    d => new PointerTensor(...d.map(i => parse(i))) // 18 = pointer-tensor
+    null, // 17
+    d => new Plan(d[0].map(j => parse(j)), ...d.slice(1).map(i => parse(i))), // 18 = plan
+    d => new PointerTensor(...d.map(i => parse(i))) // 19 = pointer-tensor
   ];
 
   const parse = d => {
