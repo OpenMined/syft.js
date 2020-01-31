da8392c25e67af7c82453ba9d996b2134ffa3673
Patrick Cason <me@patrickcason.com>
2020-01-31 17:41:13 +0000

    Update _constants.js

--- src/_constants.js
+++ src/_constants.js
@@ -22,10 +22,7 @@ export const WEBRTC_PEER_CONFIG = {
       urls: [
         'stun:stun.l.google.com:19302',
         'stun:stun1.l.google.com:19302',
-        'stun:stun2.l.google.com:19302'
-        // FF says too many stuns are bad
-        // 'stun:stun3.l.google.com:19302',
-        // 'stun:stun4.l.google.com:19302'
+        'stun:stun2.l.google.com:19302' // FF says too many stuns are bad, don't send more than this
       ]
     }
   ]
