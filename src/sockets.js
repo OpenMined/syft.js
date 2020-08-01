f46d3c2e4eecbc4fc434fb2b4a8ad2641eb33ad2
nahuakang <kangnahua@gmail.com>
2020-08-01 12:01:04 +0200

    Class and constructor level documentation

--- src/sockets.js
+++ src/sockets.js
@@ -1,6 +1,17 @@
 import { SOCKET_PING } from './_constants';
 import Logger from './logger';
 
+/**
+ * Sockets implements and wraps the WebSocket protocol for communication between client and PyGrid.
+ *
+ * @param {Object} options
+ * @param {string} options.url - Full URL to PyGrid app (`ws` and `http` schemas supported).
+ * @param {string} options.workerId - Worker ID returned from PyGrid to the client.
+ * @param {Function} [options.onOpen] - Optional function to be invoked when connection is open.
+ * @param {Function} [options.onClose] - Optional function to be invoked when connection is closed.
+ * @param {Function} options.onMessage - Optional function to be invoked when a message is received.
+ * @param {number} [options.keepAliveTimeout=20000] - Optional timeout duration to keep connection alive in milliseconds.
+ */
 export default class Sockets {
   constructor({
     url,
@@ -8,7 +19,7 @@ export default class Sockets {
     onOpen,
     onClose,
     onMessage,
-    keepAliveTimeout = 20000
+    keepAliveTimeout = 20000,
   }) {
     this.logger = new Logger();
     const socket = new WebSocket(url);
@@ -23,7 +34,7 @@ export default class Sockets {
       this.timerId = null;
     };
 
-    socket.onopen = event => {
+    socket.onopen = (event) => {
       this.logger.log(
         `Opening socket connection at ${event.currentTarget.url}`,
         event
@@ -34,7 +45,7 @@ export default class Sockets {
       if (onOpen) onOpen(event);
     };
 
-    socket.onclose = event => {
+    socket.onclose = (event) => {
       this.logger.log(
         `Closing socket connection at ${event.currentTarget.url}`,
         event
@@ -62,7 +73,7 @@ export default class Sockets {
 
       this.socket.send(JSON.stringify(message));
 
-      this.socket.onmessage = event => {
+      this.socket.onmessage = (event) => {
         const data = JSON.parse(event.data);
 
         this.logger.log('Receiving message', data);
@@ -70,7 +81,7 @@ export default class Sockets {
         resolve(this.onMessage(data));
       };
 
-      this.socket.onerror = event => {
+      this.socket.onerror = (event) => {
         this.logger.log('We have a socket error!', event);
 
         reject(event);
