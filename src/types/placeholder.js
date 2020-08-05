e08f0080ff4a577a5ac129e04fca6b7d76b80612
nahuakang <kangnahua@gmail.com>
2020-08-06 00:59:40 +0200

    Editing PlaceholderId comments; add partial Placeholder documentation

--- src/types/placeholder.js
+++ src/types/placeholder.js
@@ -4,7 +4,7 @@ import { protobuf, getPbId, pbId } from '../protobuf';
  * PlaceholderId identifies which Placeholder tensors should be used as
  * inputs and outputs of Actions inside a Plan.
  *
- * @property {string} id
+ * @property {string} id - Unbufferized Id object.
  */
 export class PlaceholderId {
   constructor(id) {
@@ -12,11 +12,12 @@ export class PlaceholderId {
   }
 
   /**
-   * Unbufferizes a protobuf object to a new PlaceholderId object.
+   * Reconstruct a PlaceholderId object from the protobuf message.
+   * Note that this method might take a worker-specific argument in the future.
    *
    * @static
-   * @param {*} worker - Reserved placeholder for worker-specific arguments when messaging with PySyft.
-   * @param {protobuf.syft_proto.types.syft.v1.Id} pb - Protobuf object.
+   * @param {*} worker - Reserved placeholder for worker-specific arguments.
+   * @param {protobuf.syft_proto.types.syft.v1.Id} pb - Protobuf object for Id.
    * @returns {PlaceholderId}
    */
   static unbufferize(worker, pb) {
@@ -24,8 +25,8 @@ export class PlaceholderId {
   }
 
   /**
-   * Bufferizes the Id string to a protobuf PlaceholderId object.
-   * Note that this method should take a worker-specific argument in the future.
+   * Save the Id string into a protobuf PlaceholderId message.
+   * Note that this method might take a worker-specific argument in the future.
    *
    * @returns {protobuf.syft_proto.execution.v1.PlaceholderId}
    */
@@ -36,6 +37,12 @@ export class PlaceholderId {
   }
 }
 
+/**
+ * Placeholder acts as a tensor. It is replaced by actual tensors after actions
+ * are acted on it.
+ *
+ * @property {string} id - Unbufferized Id object.
+ */
 export class Placeholder {
   constructor(id, tags = [], description = null, expected_shape = null) {
     this.id = id;
@@ -44,6 +51,15 @@ export class Placeholder {
     this.expected_shape = expected_shape;
   }
 
+  /**
+   * Reconstruct a Placeholder object from the protobuf message.
+   * Note that this method might take a worker-specific argument in the future.
+   *
+   * @static
+   * @param {*} worker - Reserved placeholder for worker-specific arguments.
+   * @param {protobuf.syft_proto.messaging.v1.Placeholder} pb - Protobuf object for Placeholder.
+   * @returns {Placeholder}
+   */
   static unbufferize(worker, pb) {
     let expected_shape = null;
     if (
@@ -63,6 +79,12 @@ export class Placeholder {
     );
   }
 
+  /**
+   * Bufferizes the Placeholder object to a protobuf Placeholder object.
+   * Note that this method should take a worker-specific argument in the future.
+   *
+   * @returns {protobuf.syft_proto.execution.v1.Placeholder}
+   */
   bufferize(/* worker */) {
     return protobuf.syft_proto.execution.v1.Placeholder.create({
       id: pbId(this.id),
