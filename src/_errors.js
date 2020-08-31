16853bf0119c498e99be5563552456c3c64d5053
Ollie <oleksandr@adblockplus.org>
2020-09-01 01:07:20 +0200

    Add error objects and tests for them

--- src/_errors.js
+++ src/_errors.js
@@ -27,3 +27,80 @@ export const PLAN_LOAD_FAILED = (planName, status) =>
 
 export const PROTOBUF_UNSERIALIZE_FAILED = (pbType, status) =>
   `Failed to unserialize binary protobuf data into ${pbType}: ${status}`;
+
+export class NoDetailerError extends Error {
+  constructor(d) {
+    super(NO_DETAILER(d));
+    this.name = 'NoDetailerError';
+  }
+}
+
+export class NotEnoughArgsError extends Error {
+  constructor(passed, expected) {
+    super(NOT_ENOUGH_ARGS(passed, expected));
+    this.name = 'NotEnoughArgsError';
+  }
+}
+
+export class MissingVariableError extends Error {
+  constructor() {
+    super(MISSING_VARIABLE());
+    this.name = 'MissingVariableError';
+  }
+}
+
+export class NoPlanError extends Error {
+  constructor() {
+    super(NO_PLAN);
+    this.name = 'NoPlanError';
+  }
+}
+
+export class PlanAlreadyCompletedError extends Error {
+  constructor(name, id) {
+    super(PLAN_ALREADY_COMPLETED(name, id));
+    this.name = 'PlanAlreadyCompletedError';
+  }
+}
+
+export class CannotFindCommandError extends Error {
+  constructor(command) {
+    super(CANNOT_FIND_COMMAND(command));
+    this.name = 'CannotFindCommandError';
+  }
+}
+
+export class GridUnknownCycleStatusError extends Error {
+  constructor(status) {
+    super(GRID_UNKNOWN_CYCLE_STATUS(status));
+    this.name = 'GridUnknownCycleStatusError';
+  }
+}
+
+export class GridError extends Error {
+  constructor(status) {
+    super(GRID_ERROR(status));
+    this.name = 'GridError';
+  }
+}
+
+export class ModelLoadFailedError extends Error {
+  constructor(status) {
+    super(MODEL_LOAD_FAILED(status));
+    this.name = 'ModelLoadFailedError';
+  }
+}
+
+export class PlanLoadFailedError extends Error {
+  constructor(planName, status) {
+    super(PLAN_LOAD_FAILED(planName, status));
+    this.name = 'PlanLoadFailedError';
+  }
+}
+
+export class ProtobufUnserializeFailedError extends Error {
+  constructor(pbType, status) {
+    super(PROTOBUF_UNSERIALIZE_FAILED(pbType, status));
+    this.name = 'ProtobufUnserializeFailedError';
+  }
+}
