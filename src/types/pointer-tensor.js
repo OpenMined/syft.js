2aae6849ec250088c962db0c47d5422e4a221219
nahuakang <kangnahua@gmail.com>
2020-09-25 22:02:21 +0200

    Add first iteration of inline doc to PointerTensor

--- src/types/pointer-tensor.js
+++ src/types/pointer-tensor.js
@@ -1,7 +1,19 @@
-// Create a class to represent pointer tensors
-// Add all the attributes that are serialized, just as for range and slice
-
+/**
+ * PointerTensor points to a remote tensor and forwards all API calls
+ * to its remote tensor. A PointerTensor mimic the entire API of a normal
+ * tensor, but instead of computing a tensor function (such as addition,
+ * subtraction, etc.) locally, PointerTensor forwards the computation to
+ * a remote machine as specified by self.locationId.
+ */
 export default class PointerTensor {
+  /**
+   * @param {number} id - Id of the PointerTensor.
+   * @param {number} idAtLocation - Id of the remote tensor being pointed at.
+   * @param {string} locationId - Id of the location where the remote tensor resides.
+   * @param {string} pointToAttr - String value to specify if the PointerTensor should point to an attribute of the remote tensor such as .child or .grad.
+   * @param {Array.<number>} shape - Size of the tensor the pointer points to.
+   * @param {boolean} garbageCollectData - If True, delete the remote tensor when the PointerTensor is removed.
+   */
   constructor(
     id,
     idAtLocation,
